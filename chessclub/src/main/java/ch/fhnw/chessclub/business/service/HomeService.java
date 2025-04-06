4509ac0be9d49194d20e3021258801530591bf20
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-04-06 20:24:45 +0200

    Update HomeService.java

--- chessclub/src/main/java/ch/fhnw/chessclub/business/service/HomeService.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/business/service/HomeService.java
@@ -1,1 +1,25 @@
+package ch.fhnw.chessclub.business.service;
 
+import ch.fhnw.chessclub.data.domain.HomeInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeService {
+
+    private HomeInfo info = new HomeInfo("FHNW Chess Club", "Welcome to our club platform!", "v1.0");
+
+    public HomeInfo getInfo() {
+        return info;
+    }
+
+    public HomeInfo updateInfo(HomeInfo updatedInfo) {
+        info.setTitle(updatedInfo.getTitle());
+        info.setDescription(updatedInfo.getDescription());
+        info.setVersion(updatedInfo.getVersion());
+        return info;
+    }
+
+    public void resetInfo() {
+        info = new HomeInfo("FHNW Chess Club", "Welcome to our club platform!", "v1.0");
+    }
+}
