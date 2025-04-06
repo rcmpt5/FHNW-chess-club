a078bd3b03e16ea0fcff64e824c19d3162066941
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-04-06 20:25:33 +0200

    Update HomeInfo.java

--- chessclub/src/main/java/ch/fhnw/chessclub/data/domain/HomeInfo.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/data/domain/HomeInfo.java
@@ -1,1 +1,40 @@
+package ch.fhnw.chessclub.data.domain;
 
+public class HomeInfo {
+    private String title;
+    private String description;
+    private String version;
+
+    public HomeInfo() {}
+
+    public HomeInfo(String title, String description, String version) {
+        this.title = title;
+        this.description = description;
+        this.version = version;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
