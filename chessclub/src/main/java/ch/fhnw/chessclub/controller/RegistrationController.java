2b1451fcd55abb3699cf5b042e26e1e8ea83569a
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-04-06 20:33:01 +0200

    Update RegistrationController.java

--- chessclub/src/main/java/ch/fhnw/chessclub/controller/RegistrationController.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/controller/RegistrationController.java
@@ -1,1 +1,26 @@
+package ch.fhnw.chessclub.controller;
 
+import ch.fhnw.chessclub.business.service.RegistrationService;
+import ch.fhnw.chessclub.data.domain.RegistrationForm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class RegistrationController {
+
+    @Autowired
+    private RegistrationService registrationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegistrationForm form) {
+        boolean success = registrationService.register(form);
+        if (success) {
+            return ResponseEntity.ok("Registration successful!");
+        } else {
+            return ResponseEntity.status(409).body("User already exists or data invalid.");
+        }
+    }
+}
