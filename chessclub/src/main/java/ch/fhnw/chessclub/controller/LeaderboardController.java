d1d441b290bdc3da56fa6a07fd3656477a7b13ee
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-04-06 20:36:05 +0200

    Create LeaderboardController.java

---
+++ chessclub/src/main/java/ch/fhnw/chessclub/controller/LeaderboardController.java
@@ -0,0 +1,28 @@
+package ch.fhnw.chessclub.controller;
+
+import ch.fhnw.chessclub.business.service.LeaderboardService;
+import ch.fhnw.chessclub.data.domain.LeaderboardEntry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/leaderboard")
+public class LeaderboardController {
+
+    @Autowired
+    private LeaderboardService leaderboardService;
+
+    @GetMapping
+    public List<LeaderboardEntry> getLeaderboard() {
+        return leaderboardService.getLeaderboard();
+    }
+
+    @PostMapping
+    public ResponseEntity<LeaderboardEntry> addEntry(@RequestBody LeaderboardEntry entry) {
+        return ResponseEntity.ok(leaderboardService.addEntry(entry));
+    }
+}
