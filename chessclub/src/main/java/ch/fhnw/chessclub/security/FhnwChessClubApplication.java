64883151f11d2e3d43209c03ecc485b493af23b4
Wulli25 <162603107+Wulli25@users.noreply.github.com>
2025-04-28 16:03:05 +0200

    Create FhnwChessClubApplication.java

---
+++ chessclub/src/main/java/ch/fhnw/chessclub/security/FhnwChessClubApplication.java
@@ -0,0 +1,11 @@
+package ch.fhnw.chessclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhnwChessClubApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FhnwChessClubApplication.class, args);
+  }
+}
