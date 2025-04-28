52d510cd58d951e7a8ce5d03fd87a1beabb6f536
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-04-28 16:06:01 +0200

    Update Match.java

--- chessclub/src/main/java/ch/fhnw/chessclub/data/domain/Match.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/data/domain/Match.java
@@ -1,21 +1,39 @@
 package ch.fhnw.chessclub.data.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String player1;
-    private String player2;
+
     private String result; // "1-0", "0-1", "½-½"
+
+    @ManyToOne
+    @JoinColumn(name = "player1_id", nullable = false)
+    private Player player1;
+
+    @ManyToOne
+    @JoinColumn(name = "player2_id", nullable = false)
+    private Player player2;
 
     // Getters & Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getPlayer1() { return player1; }
-    public void setPlayer1(String player1) { this.player1 = player1; }
-
-    public String getPlayer2() { return player2; }
-    public void setPlayer2(String player2) { this.player2 = player2; }
-
     public String getResult() { return result; }
     public void setResult(String result) { this.result = result; }
+
+    public Player getPlayer1() { return player1; }
+    public void setPlayer1(Player player1) { this.player1 = player1; }
+
+    public Player getPlayer2() { return player2; }
+    public void setPlayer2(Player player2) { this.player2 = player2; }
 }
