5738381fbb443f439542f5acabe7b0261465958f
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-05-19 21:41:07 +0000

    Refactor authentication to use LoginRequest class; update LoginService and LoginController accordingly

--- chessclub/src/main/java/ch/fhnw/chessclub/business/service/LoginService.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/business/service/LoginService.java
@@ -1,13 +1,14 @@
 package ch.fhnw.chessclub.business.service;
 
-import ch.fhnw.chessclub.data.domain.UserCredentials;
+import ch.fhnw.chessclub.data.domain.LoginRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 
     // Simple mock authentication (hardcoded username/password)
-    public boolean authenticate(UserCredentials credentials) {
-        return "admin".equals(credentials.getUsername()) && "chess123".equals(credentials.getPassword());
-    }
+    public boolean authenticate(LoginRequest credentials) {
+return "admin".equals(credentials.getUsername()) &&
+"chess123".equals(credentials.getPassword());
+}
 }
--- chessclub/src/main/java/ch/fhnw/chessclub/controller/LoginController.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/controller/LoginController.java
@@ -1,8 +1,7 @@
 package ch.fhnw.chessclub.controller;
 
 import ch.fhnw.chessclub.business.service.LoginService;
-import ch.fhnw.chessclub.data.domain.UserCredentials;
-
+import ch.fhnw.chessclub.data.domain.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +14,16 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserCredentials credentials) {
-        System.out.println("Username: " + credentials.getUsername());
-        System.out.println("Password: " + credentials.getPassword());
-        boolean isAuthenticated = loginService.authenticate(credentials);
-        if (isAuthenticated) {
-            return ResponseEntity.ok("Login successful!");
-        } else {
-            return ResponseEntity.status(401).body("Invalid username or password.");
-        }
-    }
+public ResponseEntity<String> login(@RequestBody LoginRequest credentials) {
+System.out.println("Username: " + credentials.getUsername());
+System.out.println("Password: " + credentials.getPassword());
+boolean isAuthenticated = loginService.authenticate(credentials);
+if (isAuthenticated) {
+    return ResponseEntity.ok("Login successful!");
+} else {
+    return ResponseEntity.status(401).body("Invalid username or password.");
 }
+
+}
+}
+
