67e78171a257408a88fb6c5ce6b644bca82a5b74
rcmpt5 <162599339+rcmpt5@users.noreply.github.com>
2025-06-02 11:49:34 +0000

    temporary fix

--- chessclub/src/main/java/ch/fhnw/chessclub/config/CorsConfig.java
+++ chessclub/src/main/java/ch/fhnw/chessclub/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package ch.fhnw.chessclub.config;
+/* package ch.fhnw.chessclub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,3 +20,4 @@ public class CorsConfig {
         };
     }
 }
+*/
